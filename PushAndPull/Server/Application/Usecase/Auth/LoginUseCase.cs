aa6f2298cae5a279a54f0be93764118f53a85518
Sean-mn <b01080080329@gmail.com>
2026-01-06 20:44:25 +0900

    feat: 로그인 UseCase 추가
    #2

---
+++ PushAndPull/Server/Application/Usecase/Auth/LoginUseCase.cs
@@ -0,0 +1,31 @@
+using Server.Application.Port.Input;
+using Server.Application.Service;
+using Server.Infrastructure.Auth;
+
+namespace Server.Application.Usecase.Auth;
+
+public class LoginUseCase : ILoginUseCase
+{
+    private readonly SteamAuthTicketValidator _validator;
+    private readonly SessionService _sessionService;
+
+    public LoginUseCase(
+        SteamAuthTicketValidator validator,
+        SessionService sessionService
+        )
+    {
+        _validator = validator;
+        _sessionService = sessionService;
+    }
+    
+    public async Task<LoginResult> ExecuteAsync(LoginCommand request)
+    {
+        var authResult = await _validator.ValidateAsync(request.Ticket);
+
+        var session = await _sessionService.CreateAsync(
+            authResult.SteamId, TimeSpan.FromDays(15)
+            );
+        
+        return new LoginResult(session.SessionId.ToString());
+    }
+}
