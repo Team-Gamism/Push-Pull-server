4e3ea19f53255492f5b535fde505e0c645a2c02f
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-01-19 21:26:03 +0900

    feat: 방 엔터티 생성
    #4

---
+++ PushAndPull/Server/Domain/Entity/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Domain.Entity;
+
+public class Room
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Password { get; set; }
+    public bool IsPrivate { get; set; }
+}
