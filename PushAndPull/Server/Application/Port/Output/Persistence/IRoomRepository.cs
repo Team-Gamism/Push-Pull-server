4f17778ad374bedf05e11da16a157c73e6139743
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-01-23 16:06:57 +0900

    feat: RoomRepository 생성
    #4

---
+++ PushAndPull/Server/Application/Port/Output/Persistence/IRoomRepository.cs
@@ -0,0 +1,11 @@
+﻿using Server.Domain.Entity;
+
+namespace Server.Application.Port.Output.Persistence;
+
+public interface IRoomRepository
+{
+    Task<Room?> GetAsync(string roomCode);
+    Task CreateAsync(Room room);
+    Task<Room> UpdateAsync(Room room);
+    Task DeleteAsync(string roomCode);
+}
---
+++ PushAndPull/Server/Infrastructure/Persistence/Repository/RoomRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Server.Application.Port.Output.Persistence;
+using Server.Domain.Entity;
+using Server.Infrastructure.Persistence.DbContext;
+
+namespace Server.Infrastructure.Persistence.Repository;
+
+public class RoomRepository : IRoomRepository
+{
+    private readonly RoomContext _context;
+    
+    public RoomRepository(RoomContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<Room?> GetAsync(string roomCode)
+    {
+        return await _context.Rooms
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.RoomCode == roomCode);
+    }
+
+    public async Task CreateAsync(Room room)
+    {
+        _context.Rooms.Add(room);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Room> UpdateAsync(Room room)
+    {
+        _context.Rooms.Update(room);
+        await _context.SaveChangesAsync();
+        return room;
+    }
+
+    public async Task DeleteAsync(string roomCode)
+    {
+        var room = await _context.Rooms
+            .FirstOrDefaultAsync(x => x.RoomCode == roomCode);
+
+        if (room == null)
+            return;
+
+        room.Status = "CLOSED";
+        room.ExpiresAt = DateTime.UtcNow;
+
+        await _context.SaveChangesAsync();
+    }
+}
