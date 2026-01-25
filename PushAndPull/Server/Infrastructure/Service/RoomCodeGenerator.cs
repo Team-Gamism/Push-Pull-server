8a65c32015f42a51e737685ed60339f62fbb6b1d
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-01-25 19:19:37 +0900

    feat: RoomCodeGenerator 생성
    #4

---
+++ PushAndPull/Server/Infrastructure/Service/RoomCodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using Server.Application.Port.Output;
+
+namespace Server.Infrastructure.Service;
+
+public class RoomCodeGenerator : IRoomCodeGenerator
+{
+    private const string AllowedChars = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789";
+    private const int CodeLength = 6;
+    
+    public string Generate()
+    {
+        var bytes = RandomNumberGenerator.GetBytes(CodeLength);
+        var chars = new char[CodeLength];
+
+        for (int i = 0; i < CodeLength; i++)
+        {
+            chars[i] = AllowedChars[bytes[i] % AllowedChars.Length];
+        }
+
+        return new string(chars);
+    }
+}
