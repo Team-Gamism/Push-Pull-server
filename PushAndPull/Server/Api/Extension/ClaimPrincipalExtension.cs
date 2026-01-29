62fcbe9b6441da9de7c35f10b54a13a98f8df5e0
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-01-30 08:55:43 +0900

    feat: 세션 인증용 ClaimsPrincipal 확장 메서드 추가
    #4

---
+++ PushAndPull/Server/Api/Extension/ClaimPrincipalExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Server.Api.Attribute;
+
+namespace Server.Api.Extension;
+
+public static class ClaimPrincipalExtension
+{
+    public static string GetSessionId(this ClaimsPrincipal user)
+    {
+        return user.FindFirst(SessionClaim.SessionId)?.Value
+               ?? throw new UnauthorizedAccessException("SESSION_ID_MISSING");
+    }
+
+    public static long GetSteamId(this ClaimsPrincipal user)
+    {
+        var value = user.FindFirst(SessionClaim.SteamId)?.Value
+                    ?? throw new UnauthorizedAccessException("STEAM_ID_MISSING");
+
+        if (!long.TryParse(value, out var steamId))
+            throw new UnauthorizedAccessException("INVALID_STEAM_ID");
+
+        return steamId;
+    }
+}
