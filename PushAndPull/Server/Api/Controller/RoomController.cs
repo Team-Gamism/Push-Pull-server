6395cc5739ac1705b0bf5b73e6313df7e141b990
PARK HYEONMIN (박현민) <b01080080329@gmail.com>
2026-02-04 06:47:38 +0900

    feat: 방입장 API 추가
    #4

--- PushAndPull/Server/Api/Controller/RoomController.cs
+++ PushAndPull/Server/Api/Controller/RoomController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Server.Api.Attribute;
 using Server.Api.Dto.Request;
 using Server.Api.Dto.Response;
+using Server.Api.Extension;
 using Server.Application.Port.Input;
 
 namespace Server.Api.Controller
@@ -11,28 +13,33 @@ namespace Server.Api.Controller
     {
         private readonly ICreateRoomUseCase _createRoomUseCase;
         private readonly IGetRoomUseCase _getRoomUseCase;
+        private readonly IJoinRoomUseCase _joinRoomUseCase;
 
         public RoomController(
             ICreateRoomUseCase createRoomUseCase,
-            IGetRoomUseCase getRoomUseCase
+            IGetRoomUseCase getRoomUseCase,
+            IJoinRoomUseCase joinRoomUseCase
             )
         {
             _createRoomUseCase = createRoomUseCase;
             _getRoomUseCase = getRoomUseCase;
+            _joinRoomUseCase = joinRoomUseCase;
         }
         
+        [SessionAuthorize]
         [HttpPost]
         public async Task<ActionResult<CreateRoomResponse>> CreateRoom(
             [FromBody] CreateRoomRequest request
             )
         {
+            var hostSteamId = User.GetSteamId();
+            
             var result = await _createRoomUseCase.ExecuteAsync(new CreateRoomCommand(
                 request.LobbyId,
                 request.RoomName,
                 request.IsPrivate,
                 request.Password,
-                request.MaxPlayers,
-                request.HostSteamId
+                hostSteamId
                 )
             );
 
@@ -64,6 +71,21 @@ namespace Server.Api.Controller
             );
             
             return Ok(response);
+        }
+
+        [SessionAuthorize]
+        [HttpPost("{roomCode}/join")]
+        public async Task<IActionResult> JoinRoom(
+            [FromRoute] string roomCode,
+            [FromBody] JoinRoomRequest request
+            )
+        {
+            await _joinRoomUseCase.ExecuteAsync(new JoinRoomCommand(
+                roomCode,
+                request.Password)
+            );
+            
+            return NoContent();
         }
     }
 }
---
+++ PushAndPull/Server/Application/UseCase/Room/JoinRoomUseCase.cs
@@ -0,0 +1,42 @@
+﻿using Server.Application.Port.Input;
+using Server.Application.Port.Output;
+using Server.Application.Port.Output.Persistence;
+using Server.Domain.Exception.Room;
+
+namespace Server.Application.UseCase.Room;
+
+public class JoinRoomUseCase : IJoinRoomUseCase
+{
+    private readonly IRoomRepository _roomRepository;
+    private readonly IPasswordHasher _passwordHasher;
+
+    public JoinRoomUseCase(
+        IRoomRepository roomRepository, 
+        IPasswordHasher passwordHasher
+        )
+    {
+        _roomRepository = roomRepository;
+        _passwordHasher = passwordHasher;
+    }
+    
+    public async Task ExecuteAsync(JoinRoomCommand request)
+    {
+        var room = await _roomRepository.GetAsync(request.RoomCode)
+            ?? throw new RoomNotFoundException(request.RoomCode);
+        
+        if (room.Status != "ACTIVE")
+            throw new RoomNotActiveException(request.RoomCode);
+
+        if (request.Password != null)
+        {
+            if (string.IsNullOrWhiteSpace(request.Password))
+                throw new InvalidOperationException("PASSWORD_REQUIRED");
+            
+            if (!_passwordHasher.Verify( request.Password, room.PasswordHash!))
+                throw new InvalidOperationException("INVALID_PASSWORD");
+        }
+
+        room.Join();
+        await _roomRepository.UpdateAsync(room);
+    }
+}
